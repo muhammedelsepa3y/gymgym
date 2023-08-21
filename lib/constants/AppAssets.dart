72ea052c02908af1b762b97f55d5ec49930fc6fa
safamahmoud71 <93884038+safamahmoud71@users.noreply.github.com>
2023-08-21 14:43:26 +0300

    favourite, home , last visite , behind you

--- lib/constants/AppAssets.dart
+++ lib/constants/AppAssets.dart
@@ -5,6 +5,11 @@ class AppAssets{
   static const String backArrow='assets/icons/backButton.svg';
 
   static const String passwordHide = 'assets/icons/passwordHide.svg';
+  static const String star = 'assets/icons/star.svg';
+  static const String favourite = 'assets/icons/favourite.svg';
+  static const String doubleArrowLeft = 'assets/icons/fi-rr-angle-double-left.svg';
+  static const String location = 'assets/icons/ic-location.svg';
+
 
 
 }
---
+++ lib/presentation/screens/favourite_screen/favourite_screen.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+import '../../../constants/size_config.dart';
+import '../../widgets/custom_appbar.dart';
+import '../../widgets/home_componant.dart';
+class FavouriteScreen extends StatelessWidget {
+  const FavouriteScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: CustomAppbar(title: "المفضلة"),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(18.0),
+          child: GridView.builder(
+
+            gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+                mainAxisExtent:270*SizeConfig.verticalBlock,
+                mainAxisSpacing:12*SizeConfig.verticalBlock,
+                crossAxisSpacing:13*SizeConfig.horizontalBlock,
+                maxCrossAxisExtent: 180
+
+
+            ),
+            shrinkWrap:true,
+            itemCount: 6,
+            itemBuilder: (BuildContext context, int index)=> const DefaultAppContainer(),
+
+          ),
+        ),
+      ),
+    );;
+  }
+}
